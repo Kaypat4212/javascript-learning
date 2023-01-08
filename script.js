65cd65d21a00b0b94a77188f16fb177a0de9a85d
Kaypat4212 <109922476+Kaypat4212@users.noreply.github.com>
2023-01-08 01:22:08 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+// let myName = "Patrick Ezike";
+// document.getElementById("information").innerHTML = myName;
+
+// let carName1 = "Bugatti";
+// let carName2 =  'Ferrari';
+
+// document.getElementById("information").innerHTML = carName1 + " " + carName2;
+
+// let word1 = "We are all going to make it";
+// let word2 = "As long as there is life, there is hope";
+// let word3 = "Life is easy, you just have to belive and be free like a bird";
+
+// document.getElementById('information').innerHTML = word1 +
+//  "<br>" + 
+//  word2 +
+//  "<br>"+ 
+//  word3;
+
+
+// length PROPERTY,
+
+// let numberOfWords = "i am patrick"
+
+// document.getElementById("information").innerHTML = numberOfWords.length;
+
+
+
